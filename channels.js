e8ecd59953ff5f72707f3f732cad354b1decc92c
mhdmurad <139557905+mhdmurad@users.noreply.github.com>
2026-07-01 23:45:45 +0600

    Update channels.js

--- channels.js
+++ channels.js
@@ -1,321 +1,55 @@
-const allChannels = [
-{
-  name: "United Sports 2",
-  logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUD6cfenW-4T2bpfkSk4QCrShs5zJvGkpHWqPE359gvs0WIgZSH87H4f4&s=10",
-  url: "https://andro.226503.xyz/checklist/androstreamliveexn2.m3u8",
-  group: "Sports"
-},
+const playlist =
+"https://raw.githubusercontent.com/sanjoykb/-KB-TV-Playlist/refs/heads/main/Github%20Auto%20Update%20Channel.m3u";
 
-{
-  name: "D SPORT",
-  logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUD6cfenW-4T2bpfkSk4QCrShs5zJvGkpHWqPE359gvs0WIgZSH87H4f4&s=10",
-  url: "https://1nyaler.streamhostingcdn.top/stream/106/index.m3u8",
-  group: "Sports"
-},
-{
-  name: "FOOTBALL",
-  logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUD6cfenW-4T2bpfkSk4QCrShs5zJvGkpHWqPE359gvs0WIgZSH87H4f4&s=10",
-  url: "https://live.teleradiocom.tj/8/3m.m3u8",
-  group: "Sports"
-},
-{
-  name: "beIN Sport",
-  logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhXse2cgIuVSVKEqzxp4g4aEV2GZUO7RIO-oHA1TKMIQ&s=10",
-  url: "https://1nyaler.streamhostingcdn.top/stream/23/index.m3u8",
-  group: "Sports"
-},
+let allChannels = [];
 
-{
-  name: "T Sports",
-  logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUD6cfenW-4T2bpfkSk4QCrShs5zJvGkpHWqPE359gvs0WIgZSH87H4f4&s=10",
-  url: "https://5c7b683162943.streamlock.net/live/ngrp:sportsone_all/playlist.m3u8",
-  group: "Sports"
-},
+document.getElementById("channels").innerHTML =
+"<div style='text-align:center;padding:40px;font-size:18px'>Loading Channels...</div>";
 
-{
-  name: "T TV Sports HD",
-  logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUD6cfenW-4T2bpfkSk4QCrShs5zJvGkpHWqPE359gvs0WIgZSH87H4f4&s=10",
-  url: "https://d3qs3d2rkhfqrt.cloudfront.net/out/v1/b17adfe543354fdd8d189b110617cddd/index_7.m3u8",
-  group: "Sports"
-}, 
-{
-  name: "FIFA 2026 ARBIC",
-  logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUD6cfenW-4T2bpfkSk4QCrShs5zJvGkpHWqPE359gvs0WIgZSH87H4f4&s=10",
-  url: "https://5c7b683162943.streamlock.net/live/ngrp:bahrainsportstwo_all/playlist.m3u8",
-  group: "Sports"
-},
-{
-  name: "TR SPORT",
-  logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiPgy6093KphJTPXUmyJANd3kZPkHhdu9bFJPz43ZBFau-RhttCYVfQk4&s=10",
-  url: "https://livetr.teleromagna.it/mia/live/playlist.m3u8",
-  group: "Sports"
-}, 
-{
-  name: "FIFA 2026",
-  logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiPgy6093KphJTPXUmyJANd3kZPkHhdu9bFJPz43ZBFau-RhttCYVfQk4&s=10",
-  url: "https://1nyaler.streamhostingcdn.top/stream/23/index.m3u8",
-  group: "Sports"
-}, 
-{
-  name: "FIFA 2026",
-  logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiPgy6093KphJTPXUmyJANd3kZPkHhdu9bFJPz43ZBFau-RhttCYVfQk4&s=10",
-  url: "https://cdn-uw2-prod.tsv2.amagi.tv/linear/amg02873-kravemedia-mtrspt1-distrotv/playlist.m3u8",
-  group: "Sports"
-},
-{
-  name: "Enter 10 Bangla",
-  logo: "https://static.wikia.nocookie.net/etv-gspn-bangla/images/4/4c/Enterr10_Bangla_logo_%282020%29.png/revision/latest?cb=20210422035753",
-  url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZ9l3CospNm55oEn0V7oc67JGpBwq5_qKfK5Xtnhfpm0WQXls0kfvpNyc&s=10",
-  group: "Sports"
-}, 
-{
-  name: "Sony Aath",
-  logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsWR3iDeYuWx1e-Ta5PWT1PT8wQ6LuLpG9ds_tPcdo2g&s",
-  url: "https://live20.bozztv.com/giatvplayout7/giatv-209611/index.m3u8",
-  group: "Sports"
-},
-{
-  name: "Cartoon Network",
-  logo: "https://upload.wikimedia.org/wikipedia/commons/8/80/Cartoon_Network_2010_logo.svg",
-  url: "https://live20.bozztv.com/giatvplayout7/giatv-209624/index.m3u8",
-  group: "Sports"
-},
-{
-  name: "GTV",
-  logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR17nZHFF6RRi0spgrD5MyEW577mND1XNi7hR_ZRJ3-41sVTXEFJhDofezG&s=10",
-  url: "https://app24.jagobd.com.bd/c3VydmVyX8RpbEU9Mi8xNy8yMFDEEHGcfRgzQ6NTAgdEoaeFzbF92YWxIZTO0U0ezN1IzMyfvcEdsEfeDeKiNkVN3PTOmdFseWRtaW51aiPhnPTI2/gazibdz.stream/tracks-v1a1/mono.m3u8",
-  group: "Bangla"
-},
+fetch(playlist)
+.then(res => res.text())
+.then(data => {
 
-{
-  name: "Channel i",
-  logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQt1L2MqYgXo0AcWVDXXlFXhY0z33fnJ9jcEA&s",
-  url: "https://server.itcnbd.live/stream/channel_i.m3u8",
-  group: "Bangla"
-},
+    const lines = data.split("\n");
 
-{
-  name: "ATN Bangla",
-  logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/ATN_Bangla_Logo_without_slogan.svg/1280px-ATN_Bangla_Logo_without_slogan.svg.png",
-  url: "https://server.itcnbd.live/stream/atn_bangla.m3u8",
-  group: "Bangla"
-},
+    for(let i = 0; i < lines.length; i++){
 
-{
-  name: "Bangla Vision",
-  logo: "https://upload.wikimedia.org/wikipedia/bn/thumb/e/e0/%E0%A6%AC%E0%A6%BE%E0%A6%82%E0%A6%B2%E0%A6%BE%E0%A6%AD%E0%A6%BF%E0%A6%B6%E0%A6%A8_%E0%A6%9F%E0%A6%BF%E0%A6%AD%E0%A6%BF%E0%A6%B0_%E0%A6%B2%E0%A7%8B%E0%A6%97%E0%A7%8B.svg/1280px-%E0%A6%AC%E0%A6%BE%E0%A6%82%E0%A6%B2%E0%A6%BE%E0%A6%AD%E0%A6%BF%E0%A6%B6%E0%A6%A8_%E0%A6%9F%E0%A6%BF%E0%A6%AD%E0%A6%BF%E0%A6%B0_%E0%A6%B2%E0%A7%8B%E0%A6%97%E0%A7%8B.svg.png",
-  url: "https://owrcovcrpy.gpcdn.net/bpk-tv/1715/output/index.m3u8",
-  group: "Bangla"
-},
+        if(lines[i].startsWith("#EXTINF")){
 
-{
-  name: "NTV",
-  logo: "https://upload.wikimedia.org/wikipedia/en/thumb/e/ef/NTV_%28Bangladesh%29_logo.svg/1280px-NTV_%28Bangladesh%29_logo.svg.png",
-  url: "https://owrcovcrpy.gpcdn.net/bpk-tv/1716/output/index.m3u8",
-  group: "Bangla"
-},
+            const info = lines[i];
+            const url = (lines[i+1] || "").trim();
 
-{
-  name: "Somoy TV",
-  logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBLRiHQqqhjBSAix_aldT3_F0DALEUsqnq5Ret-JbYdQ&s=10",
-  url: "https://server.itcnbd.live/stream/somoy_tv.m3u8",
-  group: "News"
-},
+            if(!url.startsWith("http")) continue;
 
-{
-  name: "Jamuna TV",
-  logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFoMVOyLN7Oc9VryjuPNpXfn2MDh9TJ3932M53SZ2GPDmfpuF_QSSv_2c&s=10",
-  url: "https://server.itcnbd.live/stream/jamuna_tv.m3u8",
-  group: "News"
-},
+            const name =
+                (info.match(/tvg-name="([^"]+)"/)||[])[1] ||
+                info.split(",").pop().trim();
 
-{
-  name: "Independent TV",
-  logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMDmPoSjTX1px7zI80etxqUQELIYYs6RNgRA&s",
-  url: "https://server.itcnbd.live/stream/independent_tv.m3u8",
-  group: "News"
-},
+            const logo =
+                (info.match(/tvg-logo="([^"]+)"/)||[])[1] ||
+                "https://via.placeholder.com/100?text=TV";
 
-{
-  name: "Channel 24 HD",
-  logo: "https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Logo_of_Channel_24_%28Bangladesh%29.svg/1280px-Logo_of_Channel_24_%28Bangladesh%29.svg.png",
-  url: "https://owrcovcrpy.gpcdn.net/bpk-tv/1703/output/index.m3u8",
-  group: "News"
-},
+            const category =
+                (info.match(/group-title="([^"]+)"/)||[])[1] ||
+                "Other";
 
-{
-  name: "Ekattor HD",
-  logo: "https://cdn.ekattorbd.com/contents/cache/images/1200x630x1/uploads/media/2023/08/13/default-f5a63c8327b98eafee564f28c3a9ebdb.png",
-  url: "https://owrcovcrpy.gpcdn.net/bpk-tv/1705/output/index.m3u8",
-  group: "News"
-},
+            allChannels.push({
+                name,
+                logo,
+                category,
+                url
+            });
 
-{
-  name: "STAR JALSHA HD",
-  logo: "https://upload.wikimedia.org/wikipedia/commons/e/ef/Star_Jalsha_logo_2023.png",
-  url: "https://yupptvcatchupire.yuppcdn.net/preview/starjalsha/1800.m3u8",
-  group: "Entertainment"
-},
+        }
 
-{
-  name: "SUN BANGLA HD",
-  logo: "https://static.wikia.nocookie.net/etv-gspn-bangla/images/6/60/Sun_Bangla_HD_logo.png/revision/latest?cb=20220917144006",
-  url: "https://s2.itcnbd.live/sun-bangla-hd/index.m3u8",
-  group: "Entertainment"
-},
+    }
 
-{
-  name: "ZEE BANGLA HD",
-  logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Z_Bangla_logo.jpg/500px-Z_Bangla_logo.jpg",
-  url: "https://bldcmprod-cdn.toffeelive.com/cdn/live/zee_bangla/playlist.m3u8",
-  group: "Entertainment"
-  
-},
+    showChannels(allChannels);
 
-{
-  name: "COLORS BANGLA HD",
-  logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpMjCfUm68FoC7rKtBhuAGt1HRqUfM3TxLhUSEdKITHA&s=10",
-  url: "https://yupptvcatchupire.yuppcdn.net/preview/colorsbanglahd/800.m3u8?hdnts=",
-  group: "Entertainment"
-},
+})
+.catch(()=>{
 
-{
-  name: "SONY AAT",
-  logo: "https://itcnbd.live/images/SONY AAT1745041124.jpg",
-  url: "http://192.101.68.26/CF5A68754DHEZZZZZ65698824b2f4bf69c339d68bbe8/tracks-v1a1/2026/06/29/10/11/13-06000.ts?token=44c8a1ddbb1bdf0c1b09065145a937f1",
-  group: "Entertainment"
-},
+document.getElementById("channels").innerHTML=
+"<h2 style='color:red;text-align:center'>Playlist Load Failed</h2>";
 
-{
-  name: "Zee Bangla Sonar",
-  logo: "https://itcnbd.live/images/068660250702437.Y3JvcCwxMDA3LDc4OCwxOTcsMA.jpg",
-  url: "https://s1.itcnbd.live/zee-bangla-ss/index.m3u8",
-  group: "Entertainment"
-},
-
-{
-  name: "JALSHA MOVIES HD",
-  logo: "https://upload.wikimedia.org/wikipedia/bn/7/70/%E0%A6%9C%E0%A6%B2%E0%A6%B8%E0%A6%BE_%E0%A6%AE%E0%A7%81%E0%A6%AD%E0%A6%BF%E0%A6%9C%E0%A7%87%E0%A6%B0_%E0%A6%B2%E0%A7%8B%E0%A6%97%E0%A7%8B.png",
-  url: "https://s2.itcnbd.live/jalsha-movies-hd/index.m3u8",
-  group: "Movies"
-},
-
-{
-  name: "ZEE BANGLA CINEMA",
-  logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQt-OwJERmvgjZPj7om-tvqVa5FHVxnQ3hMDJCiOnP2qjL0YEnC7Ory8s&s=10",
-  url: "https://s2.itcnbd.live/zee-bangla-cinema/index.m3u8",
-  group: "Movies"
-},
-
-{
-  name: "STAR MOVIES HD",
-  logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSduJ7hYmXCSpfO0u2btkbxQ8cXz64r8kVbshujShT3sialLGBYqXXWbuE&s=10",
-  url: "https://s2.itcnbd.live/star-movies-hd/index.m3u8",
-  group: "Movies"
-},
-
-{
-  name: "SONY PIX HD",
-  logo: "https://yt3.googleusercontent.com/nnF0vSUyYMNha7ukbxH3KeYW2gx6YVGU8rqzvURusWfxglA8cfTLtAtcIrE3uXxiNhZ-WE0Qpg=s900-c-k-c0x00ffffff-no-rj",
-  url: "https://s2.itcnbd.live/sony-pix-hd/index.m3u8",
-  group: "Movies"
-},
-
-{
-  name: "MOVIES NOW HD",
-  logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBvPixxtjtrXLs9q7SURS_705WprRWjgyYFT36QVQ3rA&s=10",
-  url: "https://s2.itcnbd.live/movies-now-hd/index.m3u8",
-  group: "Movies"
-},
-
-{
-  name: "HBO HD",
-  logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVgvpPXfAKu2RGwpiv5DGpkomLH3JMntciDbOeiwl5CzcUof8D9-Xj43q5&s=10",
-  url: "https://s2.itcnbd.live/hbo-hd/index.m3u8",
-  group: "Movies"
-},
-
-{
-  name: "Peace TV",
-  logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-fwhGz7Fm_nW6KBrLVni5LJwq17iDVCJCfFuMokDBDg&s=10",
-  url: "https://dzkyvlfyge.erbvr.com/PeaceTvBangla/tracks-v1a1/mono.m3u8",
-  group: "Islamic"
-},
-
-{
-  name: "Islamic TV",
-  logo: "https://upload.wikimedia.org/wikipedia/en/9/90/Islamic_TV_New_Logo.png",
-  url: "https://server.itcnbd.live/stream/islamic_tv.m3u8",
-  group: "Islamic"
-},
-
-{
-  name: "MADANI TV HD",
-  logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGXcVihvr9nvxPPXeS3WaZeq1AE8CQhERInRHHbToRyw&s=10",
-  url: "https://s2.itcnbd.live/madani-tv-hd/index.m3u8",
-  group: "Islamic"
-},
-
-{
-  name: "PEACE TV BANGLA",
-  logo: "https://upload.wikimedia.org/wikipedia/bn/d/d3/%E0%A6%AA%E0%A6%BF%E0%A6%B8_%E0%A6%9F%E0%A6%BF%E0%A6%AD%E0%A6%BF_%E0%A6%AC%E0%A6%BE%E0%A6%82%E0%A6%B2%E0%A6%BE.png",
-  url: "https://s2.itcnbd.live/peace-tv-bangla/index.m3u8",
-  group: "Islamic"
-},
-
-{
-  name: "Cartoon Network HD",
-  logo: "https://upload.wikimedia.org/wikipedia/commons/8/80/Cartoon_Network_2010_logo.svg",
-  url: "https://server.itcnbd.live/stream/cartoon_network_hd.m3u8",
-  group: "Kids"
-},
-
-{
-  name: "POGO",
-  logo: "https://yt3.googleusercontent.com/ytc/AIdro_mHnOf4LS2T4yeC5ru2K_NoAAMjXK6hW3bFKDCfvb1OzsM=s900-c-k-c0x00ffffff-no-rj",
-  url: "https://s2.itcnbd.live/pogo/index.m3u8",
-  group: "Kids"
-},
-
-{
-  name: "SONY YAY",
-  logo: "https://upload.wikimedia.org/wikipedia/commons/9/9a/SONY_YAY_2022_Logo.png",
-  url: "https://s2.itcnbd.live/sony-yay/index.m3u8",
-  group: "Kids"
-},
-
-{
-  name: "NICK",
-  logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Nick.png/1280px-Nick.png",
-  url: "https://s2.itcnbd.live/nick/index.m3u8",
-  group: "Kids"
-},
-
-{
-  name: "Discovery HD",
-  logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRolJlSbWJrct5RAP1Y28F3xKzALLQtNM-CGX9yJxB1LeYeD4cE3-hYeBU&s=10",
-  url: "https://s2.itcnbd.live/discovery-hd/index.m3u8",
-  group: "Documentary"
-},
-
-{
-  name: "Animal Planet HD",
-  logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp6uk_YG1ZpVnT2DMaJDimsO-PGK9jTjFmxB3W3R-SeQ&s=10",
-  url: "https://s2.itcnbd.live/animal-planet-hd/index.m3u8",
-  group: "Documentary"
-},
-
-{
-  name: "BBC EARTH HD",
-  logo: "https://upload.wikimedia.org/wikipedia/commons/3/3f/BBC_Earth.svg",
-  url: "https://s2.itcnbd.live/bbc-earth-hd/index.m3u8",
-  group: "Documentary"
-},
-
-{
-  name: "LOVE NATURE",
-  logo: "https://upload.wikimedia.org/wikipedia/commons/0/0d/Love_Nature_logo_%282025%29.svg",
-  url: "https://cdn1.logichost.in/ajmantv/live/playlist.m3u8",
-  group: "Documentary"
-},
-];
+});
