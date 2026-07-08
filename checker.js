3c76411b78d02cc3717c4b74d7e2d6c32f2c3452
mhdmurad <139557905+mhdmurad@users.noreply.github.com>
2026-07-08 09:56:36 -0700

    Update checker.js

--- checker.js
+++ checker.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 const axios = require("axios");
 
 const PLAYLISTS = [
-  "https://raw.githubusercontent.com/sanjoykb/-KB-TV-Playlist/refs/heads/main/Github%20Auto%20Update%20Channel.m3u",
   "https://raw.githubusercontent.com/shouravoo8/Tv-Channels-Network/refs/heads/main/TvChannelsnetwork.m3u",
   "https://raw.githubusercontent.com/shouravoo8/Tv-Channels-Network/refs/heads/main/100%25Freash.m3u"
 ];
